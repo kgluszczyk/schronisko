8f59c7963599e250e71854c15e951772fe3bcd08
Krzysztof Gluszczyk <gluszczykk@ryanair.com>
2021-05-25 10:19:39 +0200

    Add logs

--- app/src/main/java/pl/comarch/android/schronisko/MainActivity.kt
+++ app/src/main/java/pl/comarch/android/schronisko/MainActivity.kt
@@ -2,11 +2,38 @@ package pl.comarch.android.schronisko
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d("LIFECYCLE", "onCreate")
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.d("LIFECYCLE", "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.d("LIFECYCLE", "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.d("LIFECYCLE", "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("LIFECYCLE", "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("LIFECYCLE", "onDestroy")
     }
 }
